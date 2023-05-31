73111522eedf7f03fe751e5bbaf877d4acc97c9f
pengilton <cemal.arifogullari@web.de>
2023-05-31 14:59:05 +0200

    Implemented a sequential version of scatter shuffle

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,15 +4,38 @@
 
 using namespace std;
 
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+template<typename T>
+void foo(vector<T> &vec) {
+    for (int i = 0; i < vec.size(); i++) {
+        cout << vec[i] << " ";
+    }
+    cout << endl;
+}
 
-    for (const string& word : msg)
-    {
+template<typename T>
+void food_dumb(vector<T> vec) {
+    vec[0] = 100;
+    for (int i = 0; i < vec.size(); i++) {
+        cout << vec[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    for (const string &word: msg) {
         cout << word << " ";
     }
     cout << endl;
+
+    vector<int> numbers = {1,2,3,4,5};
+    vector<string> text = {"Hello", "World!"};
+    
+    cout << "&numbers: " << &numbers << endl;
+    foo(numbers);
+    cout << "Lets change locally smth with foo_dumb" << endl;
+    food_dumb(numbers);
+    foo(numbers);
 
     // int num[5] = {523, 458, 426, 421, 123};
     // int *numbers[5];
@@ -26,20 +49,22 @@ int main()
     //     std::cout << "*numbers[i]: " << *numbers[i] << std::endl;
     // }
 
-    int k = 2;
-    std::vector<int> *buckets[k];
-    for (int i = 0; i < k; i++) {
-        std::vector<int> bucket = {};
-        buckets[i] = &bucket;
-    }
-    buckets[0]->push_back(1);
-    buckets[1]->push_back(10);
-    buckets[1]->push_back(20);
-    buckets[0]->push_back(2);
-    buckets[0]->push_back(3);
-    for (int i = 0; i < k; i++) {
-        for (int j = 0; j < buckets[i]->size(); j++) {
-            std::cout << buckets[i]->at(j) << std::endl;
-        }
-    }
+    // int k = 2;
+    // std::vector<int> *buckets[k];
+    // for (int i = 0; i < k; i++) {
+    //     std::vector<int> bucket = {};
+    //     buckets[i] = &bucket;
+    // }
+    // buckets[0]->push_back(1);
+    // buckets[1]->push_back(10);
+    // buckets[1]->push_back(20);
+    // buckets[0]->push_back(2);
+    // buckets[0]->push_back(3);
+    // for (int i = 0; i < k; i++) {
+    //     for (int j = 0; j < buckets[i]->size(); j++) {
+    //         std::cout << buckets[i]->at(j) << std::endl;
+    //     }
+    // }
+
+    return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,37 +1,66 @@
 #include <iostream>
 #include <random>
 #include <array>
+#include <vector>
 
 // Fisher-Yates
 // Does only work with integers right now
-// template<typename T, size_t N>
-void fisher_yates_shuffle(int arr[], int size, std::default_random_engine &gen) {
-    for (int i = 0; i < size; i++) {
-        // uniform sample from [i, N-1]
-        std::uniform_int_distribution<> distrib(i, size-1);
+template<typename T>
+void fisher_yates_shuffle(std::vector<T> &vec, std::default_random_engine &gen) {
+    for (int i = 0; i < vec.size() - 1; i++) {
+        // uniform sample from [i, N)
+        std::uniform_int_distribution<> distrib(i, vec.size()-1);
         int j = distrib(gen);
 
         // swapping of values
-        auto temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        T temp = vec[i];
+        vec[i] = vec[j];
+        vec[j] = temp;
     }
 }
 
 // We will implenet the sequential version of scatter shuffle
-void scatter_shuffle(int arr[], int size, int k, std::default_random_engine &gen) {
-    int small = 32;
-    if (size <= small) {
-        fisher_yates_shuffle(arr, size, gen);
+template<typename T>
+void scatter_shuffle(std::vector<T> &vec, int k, std::default_random_engine &gen) {
+    if (vec.size() == 0) {
+        return;
     }
 
-    
+    int small = 8;
+    if (vec.size() <= small) {
+        fisher_yates_shuffle(vec, gen);
+        return;
+    }
+
+    // We have k buckets. A bucket is a vecotr for now. 
+    std::vector<T> buckets[k];
+    // Uniform distribution from 0 to k-1
+    std::uniform_int_distribution<> distrib(0, k-1);
+
+    for (int i = 0; i < vec.size(); i++) {
+        // randomly choose a bucket
+        int j = distrib(gen);
+        // copies value to chosen bucket
+        buckets[j].push_back(vec[i]);
+    }
+
+    int s = 0;
+    for (int j = 0; j < k; j++) {
+        scatter_shuffle(buckets[j], k, gen);
+        std::copy(buckets[j].begin(), buckets[j].end(), vec.begin() + s);
+        s += buckets[j].size();
+    }
 }
 
 // Function to print stuff
-void print_array(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        std::cout << arr[i] << ", ";
+template<typename T>
+void print_vec(std::vector<T> &vec) {
+    int numbers_per_line = 16;
+    for (int i = 0; i < vec.size(); i++) {
+        if (i % numbers_per_line == 0) {
+            std::cout << std::endl;
+        }
+        std::cout << vec[i] << ", ";
     }
     std::cout << std::endl;
 }
@@ -39,22 +68,25 @@ void print_array(int arr[], int size) {
 int main(int argc, char const *argv[]) {
     // Rabndom generator
     std::random_device rd;
-    std::default_random_engine generator(rd());
+    // use fixed seed to test
+    int seed = 0;
+    // int seed = rd() 
+    std::default_random_engine generator(seed);
 
-    // arbitary array
-    int size = 10;
-    int A[size];
-    for (int i = 0; i < size; i++) {
-        A[i] = i;
-    }
+    // arbitary vector
+    int size = 64;
+    //std::vector<int> V = {1,2,3,4,5,6,7,8,9,10};
+    std::vector<int> V(size);
+    std::iota(V.begin(), V.end(), 0);
 
     // print array before shuffle
-    print_array(A, size);
+    print_vec(V);
 
-    fisher_yates_shuffle(A, size, generator);
+    int k = 4;
+    scatter_shuffle(V, k, generator);
 
     // print array after shuffle
-    print_array(A, size);
+    print_vec(V);
 
     return 0;
 }
