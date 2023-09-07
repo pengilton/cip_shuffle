7eaffa0d5d99da711652757a58b84bdbd2de90a9
pengilton <cemal.arifogullari@web.de>
2023-09-08 01:42:15 +0200

    More benchmark automation
    Making it possible to run multiple vector sizes back to back. Goal is to automatically write results into a CSV file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <chrono>
 #include <cip_shuffle.hpp>
 
@@ -8,20 +9,32 @@ int main() {
 
     constexpr std::size_t num_buckets = 4;
     const std::size_t runs = 10;
-    const std::size_t size = 100000000;
+    const std::size_t max_exp = 2;
 
-    for (std::size_t i = 0; i < runs + 1; i++) {
-        std::vector<std::size_t> vec(size);
-        std::iota(vec.begin(), vec.end(), 0);
-        std::span vector_span {vec};
+    std::cout << "Starting benchmark with " << num_buckets << " buckets...\n";
 
-        auto start = std::chrono::steady_clock::now();
-        inplace_scatter_shuffle<num_buckets>(vector_span, generator);
-        auto end = std::chrono::steady_clock::now();
+    for (std::size_t i = 0; i < max_exp; i++) {
+        std::size_t size = std::pow(2, i);
+        std::cout << "\n" << "Setting size to = " << size << "\n";
 
-        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+        for (std::size_t j = 0; j < runs + 1; j++) {
+            std::vector<std::size_t> vec(size);
+            std::iota(vec.begin(), vec.end(), 0);
+            std::span vector_span {vec};
 
-        std::cout << duration.count() << " ns " << "\n";
+            auto start = std::chrono::steady_clock::now();
+            inplace_scatter_shuffle<num_buckets>(vector_span, generator);
+            auto end = std::chrono::steady_clock::now();
+
+            auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+
+            // We skip the first run as warmup.
+            if (j == 0) {
+                continue;
+            }
+
+            std::cout << "Duration of run " << std::setw(2) << j << ": " << std::setw(18) << duration.count() << " ns" << "\n";
+        }
     }
 
     return 0;
