b69ad3beec4a250c958b5cf82862bb32b6976945
pengilton <cemal.arifogullari@web.de>
2023-10-29 22:19:30 +0100

    Fixed measurement error

--- src/main.cpp
+++ src/main.cpp
@@ -33,64 +33,85 @@ std::filesystem::path create_csv_path(size_t num_buckets, size_t buffer_size, si
     return path;
 }
 
-int main() {
-    // int seed = 12345;
-    // std::mt19937_64 generator(seed);
+//----------------------------------------------------------------------------------------------------------------
+struct benchmark_param {
+    std::string function_name;
+    std::string prng_name;
+    std::size_t num_buckets;
+    std::size_t buffer_size;
+    std::size_t threshold;
+    std::size_t min_exp;
+    std::size_t max_exp;
+    std::size_t default_runs;
+    std::chrono::milliseconds min_duration;
+};
+
+void benchmark_inplace_scatter_shuffle_pcg() {
+    benchmark_param benchmark;
+    benchmark.function_name = "inplace_scatter_shuffle";
+    benchmark.prng_name = "pcg64";
+    benchmark.num_buckets = NUM_BUCKETS;
+    benchmark.buffer_size = BUFFER_SIZE;
+    benchmark.threshold = THRESHOLD;
+    benchmark.min_exp = 0;
+    benchmark.max_exp = 29;
+    benchmark.default_runs = 10;
+    benchmark.min_duration = std::chrono::milliseconds(100);
 
     pcg_extras::seed_seq_from<std::random_device> seed_source;
     pcg64 generator(seed_source);
 
-    const std::size_t runs = 10;
-    const std::size_t min_exp = 0;  //inclusive
-    const std::size_t max_exp = 30; //exclusive
-
-    std::filesystem::path path = create_csv_path(NUM_BUCKETS, BUFFER_SIZE, THRESHOLD);
+    std::filesystem::path path = create_csv_path(benchmark.num_buckets, benchmark.buffer_size, benchmark.threshold);
 
     std::fstream my_file;
     my_file.open(path, std::ios::out);
     if (my_file.is_open()) {
-        std::cout << "Starting benchmark with " << NUM_BUCKETS << " buckets...\n";
+        std::cout << "Starting benchmark with " << benchmark.num_buckets << " buckets...\n";
 
         // Creating CSV headers
-        my_file << "buckets," << "threshold," << "run," << "integers," << "duration [ns]" << "\n";
+        my_file << "function," << "prng," << "buckets," << "buffer," << "threshold," 
+                << "min_exp," << "max_exp,"
+                << "integers," << "total_runs," << "total_runtime" << "\n";
 
-        for (std::size_t i = min_exp; i < max_exp; i++) {
+        // Initiliazing vector with maximum size
+        std::vector<std::size_t> vec(std::pow(2, benchmark.max_exp));
+        std::iota(vec.begin(), vec.end(), 0);
+        std::span vector_span {vec};
+
+        for (std::size_t i = benchmark.min_exp; i <= benchmark.max_exp; i++) {
             std::size_t size = std::pow(2, i);
-            std::cout << std::setw(static_cast<size_t>(std::log10(max_exp))) << i + 1 << "/" << max_exp << " ";
-            std::cout << "Setting size = " << std::setw(static_cast<size_t>(std::log10(std::pow(2, max_exp)))) << size;
+            std::cout << std::setw(static_cast<size_t>(std::log10(benchmark.max_exp))) << i + 1 << "/" << benchmark.max_exp + 1 << " ";
+            std::cout << "Setting size = " << std::setw(static_cast<size_t>(std::log10(std::pow(2, benchmark.max_exp)))) << size;
             std::cout << " " << "which needs " << sizeof(size_t) * size << " Bytes of storage.\n";
 
-            for (std::size_t j = 0; j < runs + 1; j++) {
-                // auto start_setup = std::chrono::steady_clock::now();
-                std::vector<std::size_t> vec(size);
-                std::iota(vec.begin(), vec.end(), 0);
-                std::span vector_span {vec};
-                // auto end_setup = std::chrono::steady_clock::now();
-                // auto setup_duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end_setup - start_setup);
+            // Getting the first size elements
+            std::span view = vector_span.first(size);
 
+            std::size_t total_runs = benchmark.default_runs;
+            while (true) {
                 auto start = std::chrono::steady_clock::now();
-                inplace_scatter_shuffle(vector_span, generator);
+                for (std::size_t i = 0; i < total_runs; i++) {
+                    inplace_scatter_shuffle(view, generator);
+                }
                 auto end = std::chrono::steady_clock::now();
 
                 auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
-
-                // We skip the first run as a warmup.
-                if (j == 0) {
-                    continue;
+                if (duration >= static_cast<std::chrono::nanoseconds>(benchmark.min_duration)) {
+                    my_file << benchmark.function_name << ",";
+                    my_file << benchmark.prng_name << ",";
+                    my_file << benchmark.num_buckets << ",";
+                    my_file << benchmark.buffer_size << ",";
+                    my_file << benchmark.threshold << ",";
+                    my_file << benchmark.min_exp << ",";
+                    my_file << benchmark.max_exp << ",";
+                    my_file << size << ",";
+                    my_file << total_runs << ",";
+                    my_file << duration.count() << "\n";
+                    std::cout << "Total runtime: " << std::setw(18) << duration.count() << " ns" << "\n";
+                    break;
                 }
-
-                my_file << NUM_BUCKETS << ",";
-                my_file << THRESHOLD << ",";
-                my_file << j << ",";
-                my_file << size << ",";
-                my_file << duration.count() << "\n";
-
-                std::cout << std::setw(2) << j << "/" << runs << " ";
-                // std::cout << "Setup:   " << std::setw(18) << setup_duration.count() << " ns" << "\n";
-                // std::cout << std::setw(6) << " ";
-                std::cout << "Runtime: " << std::setw(18) << duration.count() << " ns" << "\n";
+                total_runs *= 10;
             }
-
             std::cout << "\n";
         }
 
@@ -100,6 +121,11 @@ int main() {
     } else {
         std::cout << "ERROR: File not found!" << "\n";
     }
+} 
 
+//----------------------------------------------------------------------------------------------------------------
+
+int main() {
+    benchmark_inplace_scatter_shuffle_pcg();
     return 0;
 }
