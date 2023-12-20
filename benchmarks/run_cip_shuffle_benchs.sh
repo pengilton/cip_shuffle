23f00b9c4c7ea751d63365c9d620b99840363b0d
pengilton <cemal.arifogullari@web.de>
2023-12-20 14:49:31 +0100

    More data wtih updated default threshold

--- benchmarks/run_cip_shuffle_benchs.sh
+++ benchmarks/run_cip_shuffle_benchs.sh
@@ -2,14 +2,23 @@
 
 echo "Starting benchmarks..."
 
-# ../build/src/cip_shuffle_bench_10
-# ../build/src/cip_shuffle_bench_11
-# ../build/src/cip_shuffle_bench_12
-# ../build/src/cip_shuffle_bench_13
-# ../build/src/cip_shuffle_bench_14
-# ../build/src/cip_shuffle_bench_15
-# ../build/src/cip_shuffle_bench_16
-# ../build/src/cip_shuffle_bench_17
+# ../build/src/cip_shuffle_bench_1
+# ../build/src/cip_shuffle_bench_2
+# ../build/src/cip_shuffle_bench_3
+# ../build/src/cip_shuffle_bench_4
+# ../build/src/cip_shuffle_bench_5
+# ../build/src/cip_shuffle_bench_6
+# ../build/src/cip_shuffle_bench_7
+# ../build/src/cip_shuffle_bench_8
+# ../build/src/cip_shuffle_bench_9
+../build/src/cip_shuffle_bench_10
+../build/src/cip_shuffle_bench_11
+../build/src/cip_shuffle_bench_12
+../build/src/cip_shuffle_bench_13
+../build/src/cip_shuffle_bench_14
+../build/src/cip_shuffle_bench_15
+../build/src/cip_shuffle_bench_16
+../build/src/cip_shuffle_bench_17
 ../build/src/cip_shuffle_bench_18
 ../build/src/cip_shuffle_bench_19
 ../build/src/cip_shuffle_bench_20
